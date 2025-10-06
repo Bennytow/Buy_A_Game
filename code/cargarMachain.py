33d5e0ef05cd524c982f7c6a728ebaab4985613f
Duvanzq <duvana-alarconm@unilibre.edu.co>
2025-10-06 10:34:52 -0500

    Delete code/cargarMachain.py

--- code/cargarMachain.py
+++
@@ -1,7 +0,0 @@
-# importar joblib, no sé bien qué hace pero parece que sirve para cargar el modelo
-import joblib
-modelo = joblib.load('../models/modelo_entrenado.pkl')
-#Funcion para hacer predicciones
-def predicciones(X_nuevo):
-    predicciones = modelo.predict(X_nuevo)
-    return predicciones
