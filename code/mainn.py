9415740ef5d22e52df22a12fac2b82f50906a2b5
Duvanzq <duvana-alarconm@unilibre.edu.co>
2025-10-05 21:16:24 -0500

    Update mainn.py

--- code/mainn.py
+++ code/mainn.py
@@ -1,8 +1,8 @@
-#Asi esta organizado el archivo csv
-#Rank,Name,Platform,Year,Genre,Publisher,NA_Sales,EU_Sales,JP_Sales,Other_Sales,Global_Sales
+#Esta funcion se encarga de organizar los datos de los juegos y subirlos a una lista
 def cargar_juegos(ruta_archivo:str)->list:
+#Organizacio del archivo csv
+#Rank,Name,Platform,Year,Genre,Publisher,NA_Sales,EU_Sales,JP_Sales,Other_Sales,Global_Sales
     juegos=[]
-    
     archivo=open(ruta_archivo,'r')
     titulos=archivo.readline()
     print(titulos)
