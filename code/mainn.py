21e067faaa8273b2e35c880b4b493a5c42dbfd6c
Duvanzq <duvana-alarconm@unilibre.edu.co>
2025-10-05 21:15:05 -0500

    Update mainn.py

--- code/mainn.py
+++ code/mainn.py
@@ -1,2 +1,28 @@
-  #Funcion 
+#Asi esta organizado el archivo csv
 #Rank,Name,Platform,Year,Genre,Publisher,NA_Sales,EU_Sales,JP_Sales,Other_Sales,Global_Sales
+def cargar_juegos(ruta_archivo:str)->list:
+    juegos=[]
+    
+    archivo=open(ruta_archivo,'r')
+    titulos=archivo.readline()
+    print(titulos)
+    informacion=archivo.readline()
+    
+    while len(informacion)>0:
+        info=informacion.split(',')
+        posicion=info[0]
+        nombre=info[1]
+        plataforma=info[2]
+        anio=info[3]
+        genero=info[4]
+        
+        l_juegos={'plataforma':plataforma,
+                         'nombre':nombre,
+                         'plataforma':plataforma,
+                         'anio':anio,
+                         'genero':genero}
+        juegos.append(l_canciones)
+        
+        info=archivo.readline()
+    archivo.close()
+    return juegos
